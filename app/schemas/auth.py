6e3c7e23fe048ee483f36af9ad4e2071df7eebc8
NicolasGon94 <nicolas.gonzalez@codeait.com>
2024-08-30 11:42:55 -0300

    [Implement] CI - CD and DockerFile

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional
 
-# Schema for Auth with endpoint id body schema
 class auth_schema(BaseModel):
     id: Optional[int]
     user_name: str
